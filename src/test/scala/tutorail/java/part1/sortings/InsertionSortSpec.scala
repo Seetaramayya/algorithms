371cd12c3a8efa59850c256d580293fc0bc9ffec
Seeta Ramayya Vadali <srvadali@gmail.com>
2021-05-24 13:34:14 +0200

    Sort: insertion sort

---
+++ src/test/scala/tutorail/java/part1/sortings/InsertionSortSpec.scala
@@ -0,0 +1,37 @@
+package tutorail.java.part1.sortings
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class InsertionSortSpec extends AnyWordSpec with Matchers {
+  "Insertion sort implemented in java" should {
+    "sort already sorted array as well ( best case comparisons are n-1)" in {
+      val input: Array[Comparable[_]] = Array(1, 2, 3, 4)
+      val comparison = InsertionSort.sort(input)
+      comparison shouldBe input.length - 1
+      input shouldBe Array(1, 2, 3, 4)
+    }
+
+    "sort an array with 4 elements ( best case comparisons are ∑n-1)" in {
+      val input: Array[Comparable[_]] = Array(4, 3, 2, 1)
+      val comparison = InsertionSort.sort(input)
+      comparison shouldBe 6
+      input shouldBe Array(1, 2, 3, 4)
+    }
+
+    "sort an array which is in worst case ( best case comparisons are ∑n-1)" in {
+      val n = 10
+      val input: Array[Comparable[_]] = Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+      val comparison = InsertionSort.sort(input)
+      val expectedComparisons = n * (n - 1) / 2
+      comparison shouldBe expectedComparisons
+      input shouldBe Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    }
+
+    "sort unsorted array with duplicate values" in {
+      val input: Array[Comparable[_]] = Array(3, 2, 1, 4, 4, 1, 2)
+      val comparison = InsertionSort.sort(input)
+      input shouldBe Array(1, 1, 2, 2, 3, 4, 4)
+    }
+  }
+}
